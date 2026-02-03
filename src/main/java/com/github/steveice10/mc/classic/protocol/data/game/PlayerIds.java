d45dc3809fa1e106011ca0fb4b76e8bd553fd763
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-02-03 10:33:28 -0600

    Update PlayerIds.java

--- src/main/java/com/github/steveice10/mc/classic/protocol/data/game/PlayerIds.java
+++ src/main/java/com/github/steveice10/mc/classic/protocol/data/game/PlayerIds.java
@@ -13,4 +13,34 @@ public class PlayerIds {
      * Player ID used to represent the current player when sent from a client.
      */
     public static final int SELF = 255;
+    
+    //CPE MessageTypes Ext
+
+    /**
+     * Player ID used to represent Chat
+     */
+    public static final int CHAT = 0;
+    
+    /**
+     * Player IDs used to represent Status
+     */
+    public static final int STATUS1 = 1;
+
+    public static final int STATUS2 = 2;
+
+    public static final int STATUS3 = 3;
+
+    /**
+     * Player IDs used to represent Bottom right
+     */
+    public static final int BOTTOMRIGHT1 = 11;
+
+    public static final int BOTTOMRIGHT2 = 12;
+
+    public static final int BOTTOMRIGHT3 = 13;
+
+    /**
+     * Player ID used to represent Announcement
+     */
+    public static final int ANNOUNCEMENT = 100;
 }
