b7aae3bddf410140054713137cc70ee9a2a8a294
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-02-27 08:21:55 -0600

    Add entity ID constants for various entities

---
+++ src/main/java/com/github/steveice10/mc/classic/protocol/data/game/EntityIds.java
@@ -0,0 +1,71 @@
+package com.github.steveice10.mc.classic.protocol.data.game;
+
+/**
+ * Entity ID constants.
+ */
+public class EntityIds {
+  /**
+   * Entity ID used to represent human
+  **/
+  public static final String HUMANOID = "humanoid";
+
+  /**
+   * Entity ID used to represent chicken
+  **/
+  public static final String CHICKEN = "chicken";
+
+  /**
+   * Entity ID used to represent creeper
+  **/
+  public static final String CREEPER = "creeper";
+
+  /**
+   * Entity ID used to represent crocodile
+  **/
+  public static final String CROCODILE = "croc";
+
+  /**
+   * Entity ID used to represent pig
+  **/
+  public static final String PIG = "pig";
+
+  /**
+   * Entity ID used to represent printer
+  **/
+  public static final String PRINTER = "printer";
+
+  /**
+   * Entity ID used to represent sheep
+  **/
+  public static final String SHEEP = "sheep";
+
+  /**
+   * Entity ID used to represent skeleton archer
+  **/
+  public static final String SKELETON = "skeleton";
+
+  /**
+   * Entity ID used to represent spider
+  **/
+  public static final String SPIDER = "spider";
+
+  /**
+   * Entity ID used to represent zombie
+  **/
+  public static final String ZOMBIE = "zombie";
+
+  /**
+   * Entity ID used to represent head
+  **/
+  public static final String HEAD = "head";
+
+  /**
+   * Entity ID used to represent sitting
+  **/
+  public static final String SITTING = "sitting";
+
+  /**
+   * Entity ID used to represent chibi
+  **/
+  public static final String CHIBI = "chibi";
+}
