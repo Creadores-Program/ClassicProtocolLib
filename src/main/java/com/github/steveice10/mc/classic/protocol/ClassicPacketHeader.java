bf8f14d87e8a9d6926ccd03f54927d6467c72900
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-03-15 12:06:19 -0600

    Arreglar soporte a CPE

--- src/main/java/com/github/steveice10/mc/classic/protocol/ClassicPacketHeader.java
+++ src/main/java/com/github/steveice10/mc/classic/protocol/ClassicPacketHeader.java
@@ -10,7 +10,7 @@ import java.io.IOException;
  * Packet header used by Minecraft Classic packets.
  */
 public class ClassicPacketHeader implements PacketHeader {
-    private static final int MAXLENGTH = 1035;
+    private static final int LENGTHS[] = new int[] {130, 0, 0, 1027, 6, 8, 7, 73, 9, 6, 4, 3, 1, 65, 64, 1, 66, 68, 2, 1, 2, 133, 195, 0, 2, 7, 85, 1, 3, 65, 0, 1, 7, 137, 14, 79, 1, 87, 1281, 5, 64, 5, 6, 3, 2, 2, 8, 15, 35, 25, 115, 166, 1, 65, 10, 2, 9, 4, 8, 1};
 
     @Override
     public boolean isLengthVariable() {
@@ -38,11 +38,8 @@ public class ClassicPacketHeader implements PacketHeader {
 
     @Override
     public int readPacketId(NetInput in) throws IOException {
-        if(in.available() < 1){
-            return -1;
-        }
         int id = in.readUnsignedByte();
-        if(in.available() > MAXLENGTH) {
+        if(id >= 0 && id < LENGTHS.length && LENGTHS[id] > in.available()) {
             return -1;
         }
 
