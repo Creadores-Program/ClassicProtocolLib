8062af49f6444fd12b32244c7df190959f653ddf
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-03-22 09:26:16 -0600

    Update ServerExtEntityTeleportPacket.java

--- src/main/java/com/github/steveice10/mc/classic/protocol/packet/server/ServerExtEntityTeleportPacket.java
+++ src/main/java/com/github/steveice10/mc/classic/protocol/packet/server/ServerExtEntityTeleportPacket.java
@@ -8,17 +8,17 @@ import java.io.IOException;
 
 public class ServerExtEntityTeleportPacket implements Packet {
     private int entityId;
-    private int x;
-    private int y;
-    private int z;
-    private int yaw;
-    private int pitch;
+    private float x;
+    private float y;
+    private float z;
+    private float yaw;
+    private float pitch;
 
     @SuppressWarnings("unused")
     private ServerExtEntityTeleportPacket() {
     }
 
-    public ServerExtEntityTeleportPacket(int entityId, int x, int y, int z, int yaw, int pitch) {
+    public ServerExtEntityTeleportPacket(int entityId, float x, float y, float z, float yaw, float pitch) {
         this.entityId = entityId;
         this.x = x;
         this.y = y;
@@ -27,30 +27,30 @@ public class ServerExtEntityTeleportPacket implements Packet {
         this.pitch = pitch;
     }
     public int getEntityId() { return entityId; }
-    public int getX() { return x; }
-    public int getY() { return y; }
-    public int getZ() { return z; }
-    public int getYaw() { return yaw; }
-    public int getPitch() { return pitch; }
+    public float getX() { return x; }
+    public float getY() { return y; }
+    public float getZ() { return z; }
+    public float getYaw() { return yaw; }
+    public float getPitch() { return pitch; }
 
     @Override
     public void read(NetInput in) throws IOException {
         this.entityId = in.readUnsignedByte();
-        this.x = in.readInt() / 32;
-        this.y = in.readInt() / 32;
-        this.z = in.readInt() / 32;
-        this.yaw = in.readUnsignedByte();
-        this.pitch = in.readUnsignedByte();
+        this.x = (float) (in.readInt() / 32);
+        this.y = (float) (in.readInt() / 32);
+        this.z = (float) (in.readInt() / 32);
+        this.yaw = (in.readUnsignedByte() * 360) / 256f;
+        this.pitch = (in.readUnsignedByte() * 360) / 256f;
     }
 
     @Override
     public void write(NetOutput out) throws IOException {
-        out.writeByte(this.entityId);
-        out.writeInt(this.x * 32);
-        out.writeInt(this.y * 32);
-        out.writeInt(this.z * 32);
-        out.writeByte(this.yaw);
-        out.writeByte(this.pitch);
+        out.writeByte((byte) this.entityId);
+        out.writeInt((int) (this.x * 32));
+        out.writeInt((int) (this.y * 32));
+        out.writeInt((int) (this.z * 32));
+        out.writeByte((byte) ((int) (this.yaw * 256 / 360) & 255));
+        out.writeByte((byte) ((int) (this.pitch * 256 / 360) & 255));
     }
 
     @Override
