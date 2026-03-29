f7bde055ec63f850c43eb787db1ee7d5e827f20e
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-03-28 18:43:34 -0600

    Refactor ServerBulkBlockUpdatePacket to use indices and blocks

--- src/main/java/com/github/steveice10/mc/classic/protocol/packet/server/ServerBulkBlockUpdatePacket.java
+++ src/main/java/com/github/steveice10/mc/classic/protocol/packet/server/ServerBulkBlockUpdatePacket.java
@@ -6,31 +6,33 @@ import com.github.steveice10.packetlib.packet.Packet;
 import java.io.IOException;
 
 public class ServerBulkBlockUpdatePacket implements Packet {
-    private int count;
-    private byte[] data;
+    private int[] indices;
+    private byte[] blocks;
 
     @SuppressWarnings("unused")
     private ServerBulkBlockUpdatePacket() {}
 
-    public ServerBulkBlockUpdatePacket(int count, byte[] data) {
-        this.count = count;
-        this.data = data;
+    public ServerBulkBlockUpdatePacket(int[] indices, byte[] blocks) {
+        this.indices = indices;
+        this.blocks = blocks;
     }
 
     @Override
     public void read(NetInput in) throws IOException {
-        this.count = in.readUnsignedByte();
-        this.data = in.readBytes(this.count * 4);
+        int count = (int) (in.readUnsignedByte() + 1);
+        this.indices = ClassicPacketUtil.readBulkIndices(in, count);
+        this.blocks = ClassicPacketUtil.readBulkBlocks(in, count);
     }
 
     @Override
     public void write(NetOutput out) throws IOException {
-        out.writeByte(this.count);
-        out.writeBytes(this.data);
+        out.writeByte((byte) (this.blocks.length - 1));
+        ClassicPacketUtil.writeBulkIndices(out, this.indices);
+        ClassicPacketUtil.writeBulkBlocks(out, this.blocks);
     }
 
-    public int getCount() { return count; }
-    public byte[] getData() { return data; }
+    public int[] getIndices() { return indices; }
+    public byte[] getBlocks() { return blocks; }
 
     @Override public boolean isPriority() { return false; }
 }
